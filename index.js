e1796398584468e07c671f90118ad91cffaffbdb
mingwaiTSANG <41661232+mingwaiTSANG@users.noreply.github.com>
2018-07-27 11:51:40 +0800

    Update index.js

--- index.js
+++ index.js
@@ -20,15 +20,11 @@ app.post('/webhook', (req, res) => {
     // Iterates over each entry - there may be multiple if batched
     body.entry.forEach(function(entry) {
 
-		// Gets the body of the webhook event
-		let webhook_event = entry.messaging[0];
-		console.log(webhook_event);
-
-		// Get the sender PSID
-		let sender_psid = webhook_event.sender.id;
-		console.log('Sender PSID: ' + sender_psid);
-
-	});
+      // Gets the message. entry.messaging is an array, but 
+      // will only ever contain one message, so we get index 0
+      let webhook_event = entry.messaging[0];
+      console.log(webhook_event);
+    });
 
     // Returns a '200 OK' response to all requests
     res.status(200).send('EVENT_RECEIVED');
@@ -66,18 +62,3 @@ app.get('/webhook', (req, res) => {
     }
   }
 });
-
-// Handles messages events
-function handleMessage(sender_psid, received_message) {
-
-}
-
-// Handles messaging_postbacks events
-function handlePostback(sender_psid, received_postback) {
-
-}
-
-// Sends response messages via the Send API
-function callSendAPI(sender_psid, response) {
-  
-}
